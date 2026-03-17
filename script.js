8d3cbf18fe2e598e5fec236b8386da378007af06
AdrianaGomesPimenta <adriana.agpimenta@gmail.com>
2026-03-17 17:20:17 -0300

    feat: adiciona página QA & Testes com conteúdo inicial

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-// Mapeamento de tecnologias por formação
-const tecnologias = {
-    iesb: ["Banco de Dados", "Front-End", "Redes", "Back-End",],
-    alura: ["HTML", "CSS", "JavaScript", "React", "MySQL"],
-    senai: ["HTML", "CSS", "JavaScript", "Bootstrap"]
-};
+document.addEventListener("DOMContentLoaded", () => {
 
-// Adicionando tecnologias aos cards
-document.querySelectorAll('.formacao__elemento').forEach(card => {
+  // Mapeamento de tecnologias por formação
+  const tecnologias = {
+    iesb: ["Banco de Dados", "Front-End", "Redes", "Back-End"],
+    alura: ["HTML", "CSS", "JavaScript", "React", "MySQL"],
+    bstqb: ["Casos de Teste", "Funcionais", "Exploratórios"]
+  };
+
+  // Adicionando tecnologias aos cards
+  document.querySelectorAll('.formacao__elemento').forEach(card => {
     const id = card.getAttribute('data-id');
     const techList = tecnologias[id];
     const techContainer = card.querySelector('.formacao__tecnologias');
 
-    // Adiciona as tecnologias na área designada
-    techList.forEach(tech => {
+    if (techList && techContainer) {
+      techList.forEach(tech => {
         const span = document.createElement('span');
         span.textContent = tech;
         span.style.margin = "0 8px";
         techContainer.appendChild(span);
+      });
+    }
+  });
+
+  //  CONTROLE DOS VÍDEOS (QA)
+  const videos = document.querySelectorAll('.projeto-video');
+
+  videos.forEach(video => {
+    video.addEventListener('mouseenter', () => {
+      video.play();
     });
+
+    video.addEventListener('mouseleave', () => {
+      video.pause();
+      video.currentTime = 0;
+    });
+  });
+
 });
